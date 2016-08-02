f9d0c9221b32307dba3629daef4ba37aafe2a4bd
“Krisgit config --global user.email “squeakyalien98@gmail.com <squeakyalien98@gmail.com>
2016-08-02 15:28:25 -0500

    add gem

--- app/controllers/temps_controller.rb
+++ app/controllers/temps_controller.rb
@@ -5,6 +5,7 @@ class TempsController < ApplicationController
     @temp = Unirest.get("http://api.openweathermap.org/data/2.5/weather?q=#{city},#{country}&units=imperial&APPID=4bd1acd4bf14c92a65c26dac90dbd5e8").body
     @temp_data = @temp['main']['temp']
     @location = @temp['name']
+    time = Time.new
     render 'index.html.erb'
   end
 end 
---
+++ app/models/temp.rb
@@ -0,0 +1,3 @@
+class Temp < ActiveRecord::Base
+  belongs_to :user
+end
