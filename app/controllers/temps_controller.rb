49fec74f331ca6eef74d41ff200b2b5fd3e6a3b4
“Krisgit config --global user.email “squeakyalien98@gmail.com <squeakyalien98@gmail.com>
2016-08-02 18:19:02 -0500

    fix default bug

--- app/controllers/temps_controller.rb
+++ app/controllers/temps_controller.rb
@@ -1,11 +1,10 @@
 class TempsController < ApplicationController
   def index
-    city = params['City']
-    country = params['country']
+    city = params["City"] || "Chicago"
+    country = params["country"] || "us"
     @temp = Unirest.get("http://api.openweathermap.org/data/2.5/weather?q=#{city},#{country}&units=imperial&APPID=4bd1acd4bf14c92a65c26dac90dbd5e8").body
-    @temp_data = @temp['main']['temp']
-    @location = @temp['name']
-    time = Time.new
-    render 'index.html.erb'
+    @temp_data = @temp["main"]["temp"]
+    @location = @temp["name"]
+    render 'index.html.erb' 
   end
 end 
